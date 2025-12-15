1b1f0f0c6ae85f131ad4dea75d93cb63623f4dcf
Artem Kurchenko <artem.kurchenko@devexpress.com>
2025-12-15 17:17:41 +0100

    React: JavaScript to TypeScript conversion with functional components
    - Converted class component to functional component with hooks (useCallback, useState)
    - Added proper TypeScript types for Employee and FormTypes
    - Created data.ts with typed Employee interface
    - Removed orig_ backup files
    - Build and lint checks passed

--- React/src/App.tsx
+++ React/src/App.tsx
@@ -1,17 +1,64 @@
-import { useCallback, useState } from 'react';
-import './App.css';
+import { useCallback, useMemo, useState } from 'react';
 import 'devextreme/dist/css/dx.material.blue.light.compact.css';
-import Button from 'devextreme-react/button';
+import './App.css';
+
+import Form, { SimpleItem, ButtonItem, type FormTypes } from 'devextreme-react/form';
+import TextBox, { type TextBoxTypes } from 'devextreme-react/text-box';
+import Validator, { RequiredRule } from 'devextreme-react/validator';
+import type { ButtonTypes } from 'devextreme-react/button';
+
+import validationEngine from 'devextreme/ui/validation_engine';
+import notify from 'devextreme/ui/notify';
+
+import { getEmployee, type Employee } from './data';
 
 function App(): JSX.Element {
-  var [count, setCount] = useState<number>(0);
-  const clickHandler = useCallback(() => {
-    setCount((prev) => prev + 1);
-  }, [setCount]);
+  const [employee] = useState<Employee>(getEmployee());
+
+  const lastNameRender = useCallback(
+    (data: FormTypes.SimpleItemTemplateData): JSX.Element => {
+      // eslint-disable-next-line func-style
+      const valueChanged = (e: TextBoxTypes.ValueChangedEvent): void => {
+        if (data.component && data.dataField) {
+          data.component.updateData(data.dataField, e.value);
+        }
+      };
+      return (
+        <TextBox defaultValue={employee.LastName} onValueChanged={valueChanged}>
+          <Validator validationGroup="formGroup">
+            <RequiredRule message="LastName is required" />
+          </Validator>
+        </TextBox>
+      );
+    },
+    [employee.LastName],
+  );
+
+  const photoRender = useCallback(
+    (): JSX.Element => <img id="form-avatar" src={employee.Photo} alt="employee" />,
+    [employee.Photo],
+  );
+  const validateClick = useCallback((): void => {
+    const validationResult = validationEngine.validateGroup('formGroup');
+
+    if (!validationResult.isValid) {
+      notify('dxForm is invalid', 'error', 2000);
+    }
+  }, []);
+
+  const buttonOptions: ButtonTypes.Properties = useMemo(() => ({ text: 'Validate', type: 'success', onClick: validateClick }), [validateClick]);
+
   return (
-    <div className="main">
-      <Button text={`Click count: ${count}`} onClick={clickHandler} />
-    </div>
+    <Form formData={employee} validationGroup="formGroup">
+      <SimpleItem dataField="FirstName" />
+      <SimpleItem dataField="LastName" isRequired={true} render={lastNameRender} />
+      <SimpleItem dataField="Photo" render={photoRender} />
+      <ButtonItem
+        itemType="button"
+        horizontalAlignment="left"
+        buttonOptions={buttonOptions}
+      />
+    </Form>
   );
 }
 
