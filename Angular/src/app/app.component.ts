757561700d1a2e77c3c37c6a98481e6e9dac8cb9
AlisherAmonulloev <alisher.amonulloyev@devexpress.com>
2026-05-29 14:38:39 +0300

    Migrate Angular to v21 standalone components, update ESLint flat config

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -4,8 +4,12 @@ import validationEngine from 'devextreme/ui/validation_engine';
 import notify from 'devextreme/ui/notify';
 
 import { Service, type Employee } from './app.service';
+import { DxFormModule } from 'devextreme-angular/ui/form';
+import { DxTextBoxModule } from 'devextreme-angular/ui/text-box';
+import { DxValidatorModule } from 'devextreme-angular/ui/validator';
 
 @Component({
+  imports: [DxFormModule, DxTextBoxModule, DxValidatorModule],
   selector: 'app-root',
   providers: [Service],
   templateUrl: './app.component.html',
